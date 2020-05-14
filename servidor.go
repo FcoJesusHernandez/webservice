8a4c2f563d7a2bc03ebb64b4ce4e23ecb03e56a0
Francisco <frjesus1997@gmail.com>
2020-05-13 23:13:21 -0500

    Plantilla y agregar calificacion

--- servidor.go
+++ servidor.go
@@ -4,10 +4,9 @@ import (
 	"container/list"
 	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"net/rpc"
 	"strconv"
+	"strings"
 )
 
 type Materia struct {
@@ -32,7 +31,7 @@ var lista_calificaciones list.List
 var lista_alumnos list.List
 var lista_materias list.List
 
-func (this *Calificaciones) Evaluar(datos []string, respuesta *string) error {
+func (this *Calificaciones) Evaluar(datos []string, respuesta *string, danger *bool) error {
 	alumno_ := Alumno{
 		Nombre: datos[0],
 	}
@@ -81,17 +80,20 @@ func (this *Calificaciones) Evaluar(datos []string, respuesta *string) error {
 		if !bandera {
 			lista_calificaciones.PushBack(evaluacion)
 			*respuesta = "Evaluación anexada con éxito"
+			*danger = false
 		} else {
 			*respuesta = "Error, Evaluación ya existente"
+			*danger = true
 		}
 	} else {
 		*respuesta = "Error, Evaluación debe ser numerica"
+		*danger = true
 	}
 
 	return nil
 }
 
-func (this *Calificaciones) Promedio(datos []string, respuesta *float64) error {
+func (this *Calificaciones) Promedio(datos []string, respuesta *float64, danger *bool) error {
 	var total float64
 	var promedio float64
 
@@ -137,29 +139,16 @@ func (this *Calificaciones) Promedio(datos []string, respuesta *float64) error {
 		}
 		if total == 0 {
 			*respuesta = 0
+			*danger = true
 		} else {
 			*respuesta = promedio / total
+			*danger = false
 		}
 	} else {
 		*respuesta = 0.0
+		*danger = true
 	}
 	return nil
-}
-
-func server() {
-	rpc.Register(new(Calificaciones))
-	ln, err := net.Listen("tcp", ":9999")
-	if err != nil {
-		fmt.Println(err)
-	}
-	for {
-		c, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		go rpc.ServeConn(c)
-	}
 }
 
 func form(res http.ResponseWriter, req *http.Request) {
@@ -167,11 +156,10 @@ func form(res http.ResponseWriter, req *http.Request) {
 		"Content-Type",
 		"text/html",
 	)
+	mensaje := "Calificación exitosa"
 	fmt.Fprintf(
 		res,
-		cargarHtml("form.html"),
-		alumnosHTML(),
-		materiasHTML(),
+		cargarHtml("form.html", "Agregar", mensaje, "", false, false),
 	)
 }
 
@@ -180,17 +168,135 @@ func root(res http.ResponseWriter, req *http.Request) {
 		"Content-Type",
 		"text/html",
 	)
-	fmt.Fprintf(res, cargarHtml("index.html"))
+	fmt.Fprintf(res, cargarHtml("index.html", "inicio", "Hola Bienvenido", "", false, false))
 }
 
-func cargarHtml(a string) string {
-	html, _ := ioutil.ReadFile(a)
+var clf = new(Calificaciones)
 
-	return string(html)
+func calificacion(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(res, "ParseForm() error %v", err)
+			return
+		}
+		fmt.Println(req.PostForm)
+		datos := []string{req.FormValue("alumno"), req.FormValue("materia"), req.FormValue("calificacion")}
+		var result string
+		var danger bool
+
+		clf.Evaluar(datos, &result, &danger)
+
+		res.Header().Set(
+			"Content-Type",
+			"text/html",
+		)
+
+		fmt.Fprintf(
+			res,
+			cargarHtml("index.html", "inicio", "Hola, Bienvenido", result, danger, false),
+		)
+	}
+}
+
+func promedio(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "POST":
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(res, "ParseForm() error %v", err)
+			return
+		}
+		fmt.Println(req.PostForm)
+		datos := []string{}
+		var salida string
+		var result float64
+		var danger bool
+
+		if req.FormValue("alumno") != "" {
+			salida = "El promedio de " + req.FormValue("alumno") + " es : "
+			datos = []string{"1", req.FormValue("alumno")}
+		} else if req.FormValue("materia") != "" {
+			salida = "El promedio de " + req.FormValue("materia") + " es : "
+			datos = []string{"3", req.FormValue("materia")}
+		} else {
+			salida = "Petición desconocida"
+			datos = []string{"4", "error"}
+			result = 0.0
+			danger = true
+		}
+
+		clf.Promedio(datos, &result, &danger)
+
+		res.Header().Set(
+			"Content-Type",
+			"text/html",
+		)
+
+		fmt.Fprintf(
+			res,
+			cargarHtml("index.html", "promedio", "Hola, Bienvenido", salida+fmt.Sprintf("%f", result), danger, false),
+		)
+	}
+}
+
+func promedio_gen(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(res, "ParseForm() error %v", err)
+			return
+		}
+		fmt.Println(req.PostForm)
+		datos := []string{}
+		var salida string
+		var result float64
+		var danger bool
+
+		salida = "El promedio general es : "
+		datos = []string{"2", ""}
+
+		clf.Promedio(datos, &result, &danger)
+
+		res.Header().Set(
+			"Content-Type",
+			"text/html",
+		)
+
+		fmt.Fprintf(
+			res,
+			cargarHtml("index.html", "promedio general", "Hola, Bienvenido", salida+fmt.Sprintf("%f", result), danger, false),
+		)
+	}
+}
+
+func cargarHtml(a string, titulo string, mensaje string, auxiliar string, danger_aux bool, danger_msj bool) string {
+	html, _ := ioutil.ReadFile(a)
+	salida := strings.Replace(string(html), "$__TITULO__$", titulo, -1)
+	salida = strings.Replace(salida, "$__MENSAJE__$", mensaje, -1)
+	salida = strings.Replace(salida, "$__AUXILIAR__$", auxiliar, -1)
+
+	if danger_aux {
+		salida = strings.Replace(salida, "$__CLASS_AUX__$", "alert-danger", -1)
+	} else {
+		salida = strings.Replace(salida, "$__CLASS_AUX__$", "alert-success", -1)
+	}
+
+	if danger_msj {
+		salida = strings.Replace(salida, "$__CLASS_MSJ__$", "alert-danger", -1)
+	} else {
+		salida = strings.Replace(salida, "$__CLASS_MSJ__$", "alert-secondary", -1)
+	}
+
+	//alumnosHTML(),
+	//materiasHTML(),
+	return salida
 }
 
 func main() {
-	http.HandleFunc("/", root)
+	http.HandleFunc("/calificacion", calificacion)
+	http.HandleFunc("/promedio", promedio)
+	http.HandleFunc("/general", promedio_gen)
+	http.HandleFunc("/inicio", root)
 	fmt.Println("Arrancando el servidor...")
 	http.ListenAndServe(":9000", nil)
 }
